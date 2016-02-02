59a2239a72642524ddd6483e14ea20756300a0a8
konstantin <Rhfrfpz,hf1301>
2016-02-02 12:01:30 +0200

    create custom navbar

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,27 @@
-/**
- * Created by konstantin on 26.01.16.
- */
+$(document).ready(function(){
+    $('a.toggle-dropdown').each(function(){
+        $(this).hover(function() {
+            $('.dropdown').each(function(){
 
-function follow (){
-    alert('Successful')
-}
+                var menu = $(this);
+                menu.addClass('on');
+                menu.remove();
+                $('.wrapper-download').each(function(){
+                    $(this).append(menu);
+                    $(this).mouseleave(function(){
+                        menu.removeClass('on');
+                    });
+                })
+            })
+        });
+        $(this).click(function(){
+            $('.dropdown').toggleClass('on');
+        })
+    });
+    $('a.toggle-nav').click(function() {
+        $('.collapse-nav').toggleClass('on');
+        if($('.dropdown').hasClass('on')){
+            $('.dropdown').removeClass('on')
+        }
+    });
+});
